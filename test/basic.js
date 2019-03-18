2614c8259050833440a91788f713251032ec8fc8
Marius Obert <marius.obert@sap.com>
2019-03-18 16:07:44 +0100

    Add test cases for all major config options (+fix bugs found)

--- test/basic.js
+++ test/basic.js
@@ -5,7 +5,7 @@ const execa = require('execa');
 
 describe('Basic project capabilities', function() {
 
-    describe('Generator', function() {
+    describe('Default Generator', function() {
         this.timeout(90000);
 
         it('should be able to create the project', function() {
@@ -13,11 +13,80 @@ describe('Basic project capabilities', function() {
         });
 
         it('should create the necessary ui5 files', function() {
-            return assert.file(['webapp/index.html', 'webapp/manifest.json', 'ui5.yaml']); //incomplete list, extend if necessary
+            return assert.file(['ui5.yaml', 'webapp/view/MainView.view.xml', 'webapp/index.html', 'webapp/manifest.json']); //incomplete list, extend if necessary
         });
 
         it('should create the necessary cloud foundry files', function() {
             return assert.file(['cf_deployment_resources/xs-app.json', 'cf_deployment_resources/package.json', 'mta.yaml']);
+        });
+
+        it('should create an installable project', function() {
+            return execa.shellSync('npm install')
+        });
+
+        it('should create valid code (run inner test suite)', function() {
+            return execa.shellSync('npm test')
+        });
+
+    });
+
+
+    describe('JSON Generator', function() {
+        this.timeout(90000);
+
+        it('should be able to create the project', function() {
+            return helpers.run(path.join(__dirname, '../generators/app'))
+							.withPrompts({ viewtype: 'JSON' })
+        });
+
+        it('should create the necessary ui5 files', function() {
+            return assert.file(['ui5.yaml', 'webapp/view/MainView.view.json']);
+        });
+
+        it('should create an installable project', function() {
+            return execa.shellSync('npm install')
+        });
+
+        it('should create valid code (run inner test suite)', function() {
+            return execa.shellSync('npm test')
+        });
+
+    });
+
+
+    describe('JS Generator', function() {
+        this.timeout(90000);
+
+        it('should be able to create the project', function() {
+            return helpers.run(path.join(__dirname, '../generators/app'))
+							.withPrompts({ viewtype: 'JS' })
+        });
+
+        it('should create the necessary ui5 files', function() {
+            return assert.file(['ui5.yaml', 'webapp/view/MainView.view.js']);
+        });
+
+        it('should create an installable project', function() {
+            return execa.shellSync('npm install')
+        });
+
+        it('should create valid code (run inner test suite)', function() {
+            return execa.shellSync('npm test')
+        });
+
+    });
+
+
+    describe('HTML Generator', function() {
+        this.timeout(90000);
+
+        it('should be able to create the project', function() {
+            return helpers.run(path.join(__dirname, '../generators/app'))
+							.withPrompts({ viewtype: 'HTML' })
+        });
+
+        it('should create the necessary ui5 files', function() {
+            return assert.file(['ui5.yaml', 'webapp/view/MainView.view.html']);
         });
 
         it('should create an installable project', function() {
