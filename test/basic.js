931fb88344b15eb2ca609060493d0e67fff519b4
Marius Obert <marius.obert@sap.com>
2019-10-30 09:37:12 +0100

    adapt tests

--- test/basic.js
+++ test/basic.js
@@ -3,42 +3,45 @@ const path = require('path');
 const helpers = require('yeoman-test');
 const execa = require('execa');
 
-function createTest(sTestName, oPrompt){
-	describe(sTestName, function() {
+function createTest(sTestName, oPrompt) {
+	describe(sTestName, function () {
 		this.timeout(200000);
 
-		it('should be able to create the project', function() {
+		it('should be able to create the project', function () {
 			return helpers.run(path.join(__dirname, '../generators/app')).withPrompts(oPrompt);
 		});
 
-		it('should create the necessary ui5 files', function() {
-			return assert.file(['ui5.yaml', `webapp/view/MainView.view.${oPrompt.viewtype.toLowerCase()}`, 'webapp/index.html', 'webapp/manifest.json']); //incomplete list, extend if necessary
+		it('should create the necessary ui5 files', function () {
+			return assert.file(['ui5.yaml', `webapp/view/MainView.view.${oPrompt.viewtype.toLowerCase()}`, 'webapp/index.html', 'webapp/manifest.json']);
 		});
 
-		it('should create the necessary cloud foundry files', function() {
-			return assert.file(['cf_deployment_resources/xs-app.json', 'cf_deployment_resources/package.json', 'mta.yaml']);
+		it('should create an installable project', function () {
+			return execa.commandSync('npm install');
 		});
 
-		it('should create an installable project', function() {
-			return execa.commandSync('npm install')
+		it('should create valid code (run inner test suite)', function () {
+			return execa.commandSync('npm run test');
 		});
 
-		it('should create valid code (run inner test suite)', function() {
-			return execa.commandSync('npm run test')
-		});
-
-		it('should create an buildable project', function() {
-			return execa.commandSync('npm run build:mta') //TODO won't work in static router
-		});
+		if (oPrompt.platform !== 'Static webserver') {
+			it('should create an buildable project', function () {
+				return execa.commandSync('npm run build:mta');
+			});
+		}
 
 	});
 }
 
-describe('Basic project capabilities', function() {
+describe('Basic project capabilities', function () {
 
-	createTest('XML Generator', { viewtype: 'XML' });
-	createTest('JSON Generator',{ viewtype: 'JSON', ui5libs: 'Local resources (OpenUI5)' });
+	createTest('XML Generator', { viewtype: 'XML', platform: 'Application Router @ Cloud Foundry' });
+	createTest('JSON Generator', { viewtype: 'JSON', ui5libs: 'Local resources (OpenUI5)' });
 	createTest('XML Generator', { viewtype: 'JS' });
-	createTest('XML Generator', { viewtype: 'HTML', ui5libs: 'Local resources (OpenUI5)'  });
+	createTest('XML Generator', { viewtype: 'HTML', ui5libs: 'Local resources (OpenUI5)', platform: 'Application Router @ Cloud Foundry' });
+
+	createTest('XML Generator', { viewtype: 'XML', platform: 'Application Router @ SAP HANA XS Advanced' });
+	createTest('JSON Generator', { viewtype: 'JSON', ui5libs: 'Local resources (OpenUI5)', platform: 'Application Router @ SAP HANA XS Advanced' });
+	createTest('XML Generator', { viewtype: 'JS', platform: 'Cloud Foundry HTML5 Application Repository' });
+	createTest('XML Generator', { viewtype: 'HTML', ui5libs: 'Local resources (OpenUI5)', platform: 'Application Router @ SAP HANA XS Advanced' });
 
 });
