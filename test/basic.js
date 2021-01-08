9d5fa6033093cf719c0f263f83fb0b0d59484ab3
Marius Obert <marius.obert@sap.com>
2021-01-08 11:40:34 +0100

    point to right xs-app file, fixes #57

--- test/basic.js
+++ test/basic.js
@@ -19,6 +19,12 @@ function createTest(oPrompt) {
       return assert.fileContent("uimodule/webapp/controller/MainView.controller.js", "controller/BaseController");
     });
 
+    if (!!oPrompt.platform && oPrompt.platform !== "Static webserver" && oPrompt.platform !== "SAP NetWeaver") {
+      it("ui5.yaml middleware should point to the right xs-app.json file", function () {
+        return assert.fileContent("uimodule/ui5.yaml", oPrompt.platform === "Application Router @ Cloud Foundry" ? "xsappJson: approuter/xs-app.json" : "xsappJson: uimodule/webapp/xs-app.json");
+      });
+    }
+
     it("should create an installable project", function () {
       return execa.commandSync("npm install");
     });
