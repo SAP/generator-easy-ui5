f2d5da8b36d7d566922a97b075dd72f342dd5e3a
tsaleksandrova <tsvetelina.aleksandrova@sap.com>
2021-02-16 17:09:26 +0200

    chore: uiveri5 - add params for saucelabs reporter

--- generators/uiveri5/templates/conf.js
+++ generators/uiveri5/templates/conf.js
@@ -2,7 +2,11 @@ exports.config = {
   profile: "integration",
   baseUrl: "<%= baseUrl %>"<% if (chosenReporters.length) { %>,
   reporters: [<% chosenReporters.forEach(function (reporter) { %>
-    {name: "./reporter/<%= reportersMap[reporter] %>"},<% }) %>
+    {
+      name: "./reporter/<%= reportersMap[reporter] %>"<% if (reporter === "SauceLabs") { %><% if (saucelabsResultsUrl) { %>,
+      resultsUrl: "<%= saucelabsResultsUrl %>"<% } %><% if (saucelabsLoginUrl) { %>,
+      loginUrl: "<%= saucelabsLoginUrl %>"<% } %><% } %>
+    },<% }) %>
   ]<% } %><% if (viewtype) { %>,
   specs: ["./<%= dirname %>/*.spec.js"]<% } %><% if (auth && authMap[auth]) { %>,
   auth: {
@@ -12,5 +16,23 @@ exports.config = {
       user: "${params.user}",
       pass: "${params.pass}"
     }
-  }<% } %>
+  }<% } %><% if (chosenReporters.indexOf("SauceLabs") > -1) { %>,
+
+  // open the browser on SauceLabs cloud
+  seleniumAddress: "https://<user>:<key>@ondemand.eu-central-1.saucelabs.com:443/wd/hub",
+
+  browsers: [{
+    // define the runtime according UIVeri5 naming scheme
+    browserName: "edge",
+    platformName: "windows",
+    capabilities: {
+      // override runtime with SauceLabs specific names
+      platform: "Windows 10",
+      browserName: "MicrosoftEdge",
+      // add details for your SauceLabs test execution
+      name: "my-test",
+      tags: ["UIVeri5"],
+      build: process.env.BUILD_NUMBER
+    }
+  }]<% } %>
 };
